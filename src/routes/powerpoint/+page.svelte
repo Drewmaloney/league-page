dceafce41566c41aa9f697a237932d9356254289
Drewmaloney <158063108+Drewmaloney@users.noreply.github.com>
2024-04-06 12:15:52 -0400

    Create +page.svelte
    Adding powerpoint tab info with presentations.

---
+++ src/routes/powerpoint/+page.svelte
@@ -0,0 +1,8 @@
+<p> 2023-2024 Preseason Presentation</p>
+<iframe src="https://1drv.ms/p/c/fd44bd96dcba0998/IQPH-YDBJifMQ78saxilxej1AWEETvFZqcqYr2vZc6O13kY?em=2&amp;wdAr=1.7777777777777777" width="750px" height="500px" frameborder="0">This is an embedded <a target="_blank" href="https://office.com">Microsoft Office</a> presentation, powered by <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>
+
+
+<p> 2022-2023 Preseason Presentation</p>
+<iframe src="https://1drv.ms/p/c/fd44bd96dcba0998/IQM4QKbA7YWBRJ0YKpuBPwWuAQE5QzNqpdIi1wz3b6YShGo?em=2&amp;wdAr=1.7777777777777777" width="750px" height="500px" frameborder="0">This is an embedded <a target="_blank" href="https://office.com">Microsoft Office</a> presentation, powered by <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>
+
+
