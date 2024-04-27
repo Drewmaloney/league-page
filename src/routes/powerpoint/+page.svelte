e3980324c4664b2e6232a06b43148657df7f1e97
Drewmaloney <158063108+Drewmaloney@users.noreply.github.com>
2024-04-27 15:45:24 -0400

    Update +page.svelte
    Add 24-25 Preseason Presentation

--- src/routes/powerpoint/+page.svelte
+++ src/routes/powerpoint/+page.svelte
@@ -1,3 +1,6 @@
+<p> 2024-2025 Preseason Presentation</p>
+<iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQuyamGKYpo_oEiedviNM-ODL0Cbwz-XSVsVSTkfPoXonWbT0ZN2SN8VDRGExGdZlWMUt0G9i7Nlg2b/embed?start=false&loop=false&delayms=3000" frameborder="0" width="750" height="500" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+
 <p> 2023-2024 Preseason Presentation</p>
 <iframe src="https://1drv.ms/p/c/fd44bd96dcba0998/IQPH-YDBJifMQ78saxilxej1AWEETvFZqcqYr2vZc6O13kY?em=2&amp;wdAr=1.7777777777777777" width="750px" height="500px" frameborder="0">This is an embedded <a target="_blank" href="https://office.com">Microsoft Office</a> presentation, powered by <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>
 
